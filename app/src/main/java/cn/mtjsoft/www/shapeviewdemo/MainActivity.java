48179b420199c293ff33d242469121c60ef1134e
matengjiao <matj@csizg.com>
2022-01-14 11:18:32 +0800

    fix(app):修改示例

--- app/src/main/java/cn/mtjsoft/www/shapeviewdemo/MainActivity.java
+++ app/src/main/java/cn/mtjsoft/www/shapeviewdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package cn.mtjsoft.www.shapeviewdemo;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -15,8 +18,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                Window window = getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+                window.getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         setContentView(R.layout.activity_main);
-
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(R.string.app_name);
+        }
         shapeTextView = findViewById(R.id.stv);
         // 代码动态设置
         shapeTextView.setBuilder(
